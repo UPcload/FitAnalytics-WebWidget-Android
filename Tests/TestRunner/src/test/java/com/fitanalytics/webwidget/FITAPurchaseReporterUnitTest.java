a7c1ebad07222c191e3c01d15dfe49d1d8f22812
Heiko Ehrig <ehrig@fitanalytics.com>
2019-01-30 12:01:25 +0100

    make sendReport package-private

--- Tests/TestRunner/src/test/java/com/fitanalytics/webwidget/FITAPurchaseReporterUnitTest.java
+++ Tests/TestRunner/src/test/java/com/fitanalytics/webwidget/FITAPurchaseReporterUnitTest.java
@@ -6,76 +6,108 @@ import org.junit.Test;
 
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class FITAPurchaseReporterUnitTest {
 
     FITAPurchaseReporter reporter;
     private FITAPurchaseReport report;
+    private FITAPurchaseReport reportMinimal;
+    private FITAPurchaseReport reportFull;
+
 
     @Before
     public void setup(){
         reporter = new FITAPurchaseReporter();
-        report = new FITAPurchaseReport("test-12345","12345", 0.001d, "EUR");
+
+        report = new FITAPurchaseReport("test-12345","12345", "XXL-oversize", 0.01d, "EUR");
+
+        reportMinimal = new FITAPurchaseReport("12345-minimal","1234minimal");
+
+        reportFull = new FITAPurchaseReport("12345full","1234minimal");
+        reportFull.setPurchasedSize("XL");
+        reportFull.setEan("1234567890123");
+        reportFull.setFunnel("ab-funnel");
+        reportFull.setShop("anothershop");
+        reportFull.setShopArticleCode("1222-shoparticlecode");
+        reportFull.setShopCountry("DE");
+        reportFull.setShopLanguage("de");
+        reportFull.setShopSizingSystem("shopSizingSystem-region-europe");
+        reportFull.setSid("shop-sessionid-1234");
+        reportFull.setSizeRegion("sizeRegion");
+
+        reporter.setTestEnv(true);
 
     }
+
 
 
 
     @Test
     public void testToString(){
-        assertEquals("FITAReporter: REPORT_URL=https://collector.fitanalytics.com/purchases"+
-                " REPORT_URL2=https://collector-de.fitanalytics.com/purchases",reporter.toString());
+        assertEquals("com.fitanalytics.webwidget.FITAPurchaseReporter Object {\n" +
+                "  FITA_WIDGET: fitaWidget\n" +
+                "  REPORT_URL1: https://collector.fitanalytics.com/purchases\n" +
+                "  REPORT_URL2: https://collector-de.fitanalytics.com/purchases\n" +
+                "  USER_AGENT: Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1-FitAnalytics v0.0.1\n" +
+                "  isDryRun: false\n" +
+                "  isTestEnv: true\n" +
+                "}",reporter.toString());
     }
 
+
+    @Test
+    public void testGetCookie(){
+        String connect_sid = reporter.getCookieValue("__cfduid=d548b30461fe3b3c3cc709ea30a02c98d1546606472; connect.sid=s%3AqFuFsWjiRozo_BUnOPpJgPmJ3pSOr3OH.DSjQsAcIpNl2kjc4M9n4BVHOJTiV8sg0CuVqMwGy85s").get("connect.sid");
+        assertEquals("s%3AqFuFsWjiRozo_BUnOPpJgPmJ3pSOr3OH.DSjQsAcIpNl2kjc4M9n4BVHOJTiV8sg0CuVqMwGy85s",connect_sid);
+    }
     @Test
     public void testReport(){
         assertEquals("test-12345", report.getProductSerial());
         assertEquals("12345", report.getOrderId());
-        assertEquals(new Double(0.001d), report.getPrice());
+        assertEquals(new Double(0.01d), report.getPrice());
         assertEquals("EUR", report.getCurrency());
 
+        assertEquals("12345-minimal", reportMinimal.getProductSerial());
+        assertEquals("1234minimal", reportMinimal.getOrderId());
+        assertNull(reportMinimal.getPrice());
+        assertNull(reportMinimal.getCurrency());
+
+
     }
+
+
     @Test
     public void testProcessReport() {
 
         reporter.setDryRun(true);
-        assertEquals(true, reporter.isDryRun());
+        assertTrue(reporter.isDryRun());
 
         report.setProductSerial("");
-        assertEquals(true, reporter.processReport(report).isEmpty());
+        assertTrue(reporter.processReport(report).isEmpty());
 
         report.setProductSerial("test-12345");
-        assertEquals(false, reporter.processReport(report).isEmpty());
-
-
+        assertFalse(reporter.processReport(report).isEmpty());
+        // we are in dry-run
         assertEquals("test-test-12345", reporter.processReport(report).get("productId"));
 
         reporter.setDryRun(false);
         assertEquals("test-12345", reporter.processReport(report).get("productId"));
-        assertEquals("0.001", reporter.processReport(report).get(FITAPurchaseReporter.KEYS.PRICE));
+        assertEquals("0.01", reporter.processReport(report).get(FITAPurchaseReporter.KEYS.PRICE));
         assertEquals("EUR", reporter.processReport(report).get(FITAPurchaseReporter.KEYS.CURRENCY));
         assertEquals("12345", reporter.processReport(report).get(FITAPurchaseReporter.KEYS.ORDER_ID));
-        long reporterepocheTimeInMs = Long.parseLong((String)reporter.processReport(report).get(FITAPurchaseReporter.KEYS.TIMESTAMP));
+        assertEquals("XXL-oversize", reporter.processReport(report).get(FITAPurchaseReporter.KEYS.PURCHASED_SIZE));
+
+
+        long reporterepocheTimeInMs = Long.parseLong((String)reporter.processReport(report).get(FITAPurchaseReporter.KEYS.TIME_STAMP));
         long epocheTimeInMS = new Date().getTime();
         assertEquals(true, epocheTimeInMS>=reporterepocheTimeInMs);
         assertEquals(FITAPurchaseReporter.KEYS.EMBED_ANDROID, reporter.processReport(report).get(FITAPurchaseReporter.KEYS.HOSTNAME));
-    }
-    @Test
-    public  void testSendReport(){
-        reporter.setDryRun(true);
-        report.setProductSerial(null);
-        boolean result = reporter.sendReport(report);
-        assertEquals(false, result);
-
-        report.setProductSerial("test-54321");
-        result = reporter.sendReport(report);
-        assertEquals(true, result);
-
-        report.setProductSerial("test54321");
-        result = reporter.sendReport(report);
-        assertEquals(true, result);
 
 
     }
+
 }
--- webwidget/src/main/java/com/fitanalytics/webwidget/FITAPurchaseReporter.java
+++ webwidget/src/main/java/com/fitanalytics/webwidget/FITAPurchaseReporter.java
@@ -1,29 +1,66 @@
 package com.fitanalytics.webwidget;
 
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.webkit.CookieManager;
+import android.webkit.WebSettings;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Date;
-import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
 
-public class FITAPurchaseReporter {
+public class FITAPurchaseReporter extends AsyncTask<FITAPurchaseReport, Integer, Long> {
 
 
+    public static final String FITA_WIDGET = "fitaWidget";
     private final static String REPORT_URL1 = "https://collector.fitanalytics.com/purchases";
     private final static String REPORT_URL2 = "https://collector-de.fitanalytics.com/purchases";
-    private static final String USER_AGENT = "Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1-testRunner";
-    private CookieManager cookieManager ;
+    /***
+     * Some CDN block USER_AGENT based on java, so we need to create our own.
+     */
+    public static String USER_AGENT = "Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1-FitAnalytics v0.0.1";
+    private boolean isDryRun = false;
+    private boolean isTestEnv = false;
+
+
+    public FITAPurchaseReporter() {
+    }
+
+    public FITAPurchaseReporter(Context context) {
+        try {
+            USER_AGENT = WebSettings.getDefaultUserAgent(context);
+        } catch (Exception e) {
+            Log.d(FITA_WIDGET, "Could not set the USER AGENT from the context provided", e);
+        }
+    }
+
+    @Override
+    protected Long doInBackground(FITAPurchaseReport... fitaPurchaseReports) {
+        int count = fitaPurchaseReports.length;
+        long totalSize = 0;
+
+        for (int i = 0; i < count; i++) {
+            totalSize += (sendReport(fitaPurchaseReports[i])?1:0);
+            publishProgress((int) ((i / (float) count) * 100));
+
+            if (isCancelled()) break;
+        }
+        return totalSize;
+    }
+
 
     public boolean isDryRun() {
         return isDryRun;
@@ -33,10 +70,8 @@ public class FITAPurchaseReporter {
         isDryRun = dryRun;
     }
 
-    private boolean isDryRun = false;
-
-    public FITAPurchaseReporter() {
-        // TODO cookieManager & access to cokkies from webview
+    public void setTestEnv(boolean testEnv) {
+        isTestEnv = testEnv;
     }
 
 
@@ -46,62 +81,90 @@ public class FITAPurchaseReporter {
      * @return true if successful false otherwise
      */
 
-    public boolean sendReport(FITAPurchaseReport report){
-        Dictionary reportAs = processReport (report);
+    boolean sendReport(FITAPurchaseReport report){
+        Map<String, String> reportAs = processReport (report);
         boolean result = false;
 
         if (reportAs.size()==0){
             return result;
         }
-        result = sendRequest(REPORT_URL1, reportAs);
-        boolean result2=  sendRequest(REPORT_URL2, reportAs);
-        return (result && result2);
+        if (!isTestEnv) {
+            result = sendRequest(REPORT_URL1, reportAs);
+            sendRequest(REPORT_URL2, reportAs);
+        } else {
+            result = sendRequest(REPORT_URL2, reportAs);
+        }
+
+        return (result);
     }
 
-
-
-    private boolean sendRequest(String uri, Dictionary reportAsDic ) {
+    /**
+     * fires a report to the collector endpoint
+     * silently accepts
+     * @param uri
+     * @param reportAsDictionary
+     * @return
+     */
+    private boolean sendRequest(String uri, Map<String, String> reportAsDictionary) {
         URL url = null;
+        int responseCode = 0;
+
         boolean isSuccess = false;
         try {
+            String query = getQuery(reportAsDictionary);
+            uri += "?" + query;
             url = new URL(uri);
-        HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
             try {
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setDoInput(true);
-                urlConnection.setDoOutput(true);
-                // TODO get a correct agent String for an android client without using the webviewcontext
-                // using:  WebSettings.getDefaultUserAgent(context);
-                // Cloudflare rejects USer-Agent "Java/xxx"
+                urlConnection.setRequestMethod("GET");
+                // Cloudflare rejects User-Agent "Java/xxx"
                 urlConnection.setRequestProperty("User-Agent", USER_AGENT);
+                responseCode = urlConnection.getResponseCode();
+                if (isTestEnv) {
+                    boolean isPurchaseRecorded = getResponse(urlConnection);
+                    if (isPurchaseRecorded)
+                    {
+                        Log.d(FITA_WIDGET, "Purchase is successfully sent , productSerial is " +reportAsDictionary.get("productSerial"));
+                    }
+                }
 
-                OutputStream os = urlConnection.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-
-                String query = getQuery((Hashtable<String, String>) reportAsDic);
-                writer.write(query);
-                writer.flush();
-                writer.close();
-
-                os.close();
-                urlConnection.connect();
             } finally {
-                int responseCode = urlConnection.getResponseCode();
                 urlConnection.disconnect();
-                return (responseCode == 200);
+                isSuccess = (responseCode == 200);
             }
         } catch (MalformedURLException e) {
-            e.printStackTrace();
+            Log.e(FITA_WIDGET, "Mailformed collector url");
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.d(FITA_WIDGET, "Could not send the report with productSerial: " +reportAsDictionary.get("productSerial"));
         }
-        return false;
+        return isSuccess;
     }
 
 
+    /**
+     * The collector responds with a "purchase was collected" if the purchase was succesfully recorded
+     */
+    private boolean getResponse(HttpsURLConnection urlConnection) throws IOException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(urlConnection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
 
-    private String getQuery(Hashtable<String,String> params) throws UnsupportedEncodingException {
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return (response.toString().equals("purchase was collected"));
+    }
+
+
+    /**
+     * helper method to create a query
+     * @param params
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    private String getQuery(Map<String,String> params) throws UnsupportedEncodingException {
         StringBuilder result = new StringBuilder();
         boolean first = true;
         for ( String key : params.keySet())
@@ -120,10 +183,11 @@ public class FITAPurchaseReporter {
     }
 
     /**
+     *
      * TODO : make this JAVA_8  compatible
      */
-    Dictionary<String,String> processReport (FITAPurchaseReport report) {
-        Hashtable<String, String> dict = new Hashtable<String, String>();
+    Map<String,String> processReport (FITAPurchaseReport report) {
+        Map<String, String> dict = new Hashtable<String, String>();
         String productSerial = report.getProductSerial();
 
         if (productSerial != null && !productSerial.equals("")) {
@@ -146,45 +210,53 @@ public class FITAPurchaseReporter {
             putIfNotNull(KEYS.CURRENCY,report.getCurrency(), dict);
             putIfNotNull(KEYS.EAN,report.getEan(), dict);
             putIfNotNull(KEYS.FUNNEL,report.getFunnel(), dict);
-            putIfNotNull(KEYS.HOSTNAME,report.getHostname(), dict);
-
-            putIfNotNull(KEYS.PRICE,report.getPrice().toString(), dict);
+            Double price = report.getPrice();
+            putIfNotNull(KEYS.PRICE,price==null?null:price.toString(), dict);
 
             putIfNotNull(KEYS.SHOP_COUNTRY,report.getShopCountry(), dict);
             putIfNotNull(KEYS.SHOP_LANGUAGE,report.getShopLanguage(), dict);
             putIfNotNull(KEYS.SHOP_SIZING_SYSTEM,report.getShopSizingSystem(), dict);
 
+            String connect_sid =report.getSid();
 
-           //TODO sessionid
-            putIfNotNull(KEYS.SID,report.getSid(), dict);
+            try {
+                String cookies = CookieManager.getInstance().getCookie(FITAWebWidget.widgetContainerURL);
+                connect_sid = getCookieValue(cookies).get("connect.sid");
+            } catch (NullPointerException ex) {
+                // we are not in an web env.
+            }finally {
+                putIfNotNull(KEYS.SID,connect_sid, dict);
+            }
 
             putIfNotNull(KEYS.SIZE_REGION,report.getSizeRegion(), dict);
 
-            dict.put(KEYS.SENDER,"callback");
+            dict.put(KEYS.SENDER_TYPE,"callback");
             dict.put(KEYS.HOSTNAME, KEYS.EMBED_ANDROID);
             long epocheTimeInMS = new Date().getTime();
-            dict.put(KEYS.TIMESTAMP, ""+epocheTimeInMS);
+            dict.put(KEYS.TIME_STAMP, ""+epocheTimeInMS);
             // TODO JAVA 8+
             //long unixTimestamp = Instant.now().getEpochSecond();
 
         }
-
-        /**
-
-         *     NSString *sid = nil;
-         *     if (report.sid != nil)
-         *         sid = report.sid;
-         *     else
-         *         sid = [_defaults objectForKey:kSidKey];
-         *
-         *     if (sid != nil && [sid isKindOfClass:[NSString class]])
-         *         [dict setValue:sid forKey:@"sid"];
-         *
-         */
         return dict;
     }
 
-    private void putIfNotNull(String key, String value, Hashtable<String, String> dict) {
+
+    Map<String, String> getCookieValue(String cookieHeader) {
+            Map<String, String> result = new HashMap<String, String>();
+            String[] cookies = cookieHeader.split( "; " );
+            for ( int i = 0; i < cookies.length; i++ ) {
+                String cookie = cookies[ i ];
+                String key = cookie.split( "=" )[ 0 ];
+
+                String value = cookie.substring( cookie.indexOf( '=' ) + 1, cookie.length() );
+                result.put( key, value );
+            }
+            return result;
+        }
+
+
+    private void putIfNotNull(String key, String value, Map<String, String> dict) {
         if (value!=null){
             dict.put(key,value);
         }
@@ -193,29 +265,59 @@ public class FITAPurchaseReporter {
 
     @Override
     public String toString(){
-        return "FITAReporter: REPORT_URL="+REPORT_URL1 +" REPORT_URL2=" + REPORT_URL2 ;
-    }
+            StringBuilder result = new StringBuilder();
+            String NL = System.getProperty("line.separator");
+
+            result.append(this.getClass().getName());
+            result.append(" Object {");
+            result.append(NL);
+
+            //determine fields declared in this class only (no fields of superclass)
+            Field[] fields = this.getClass().getDeclaredFields();
+
+            //print field names paired with their values
+            for (Field field : fields) {
+                result.append("  ");
+                try {
+                    result.append(field.getName());
+                    result.append(": ");
+                    //requires access to private field:
+                    result.append(field.get(this));
+                }
+                catch (IllegalAccessException ex) {
+                    // do nothing
+                }
+                result.append(NL);
+            }
+            result.append("}");
+
+            return result.toString();
+        }
 
 
-    class KEYS {
-        static final String PRODUCT_ID = "productId";
-        static final String VARIANT_ID = "variantId";
-        static final String HOSTNAME = "hostname";
-        static final String ORDER_ID = "orderId";
-        static final String USER_ID = "userId";
-        static final String PURCHASED_SIZE = "purchasesSize";
-        static final String SHOP = "shop";
-        static final String CURRENCY = "currency";
-        static final String EAN = "ean";
-        static final String FUNNEL = "funnel";
-        static final String PRICE = "price";
-        static final String SHOP_COUNTRY = "shopCountry";
-        static final String SHOP_LANGUAGE = "shopLanguage";
-        static final String SHOP_SIZING_SYSTEM = "shopSizingSystem";
-        static final String SID = "sid";
-        static final String SIZE_REGION = "sizeRegion";
-        static final String SENDER = "sender";
-        static final String TIMESTAMP = "timestamp";
+    /**
+     * provides easy access to possible values to report
+     */
+    public class KEYS {
+        public static final String PRODUCT_ID = "productId";
+        public static final String VARIANT_ID = "variantId";
+        public static final String HOSTNAME = "hostname";
+        public static final String ORDER_ID = "orderId";
+        public static final String USER_ID = "userId";
+        public static final String PURCHASED_SIZE = "purchasedSize";
+        public static final String SHOP = "shop";
+        public static final String CURRENCY = "currency";
+        public static final String EAN = "ean";
+        public static final String FUNNEL = "funnel";
+        public static final String PRICE = "price";
+        public static final String SHOP_COUNTRY = "shopCountry";
+        public static final String SHOP_LANGUAGE = "shopLanguage";
+        public static final String SHOP_SIZING_SYSTEM = "shopSizingSystem";
+        public static final String SID = "sid";
+        public static final String SIZE_REGION = "sizeRegion";
+        public static final String SENDER_TYPE = "senderType";
+        public static final String TIME_STAMP = "timeStamp";
+
         static final String EMBED_ANDROID = "embed-android";
     }
 }
